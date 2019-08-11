582fec9ddbf7b688ac6e20e95c5ab9b507577529
Jakob Rolik <rolik.jakob@gmail.com>
2019-08-11 16:41:13 +0200

    Connected form with state

--- wp-content/themes/portfolio/react-src/src/components/ContactMe/Form/index.tsx
+++ wp-content/themes/portfolio/react-src/src/components/ContactMe/Form/index.tsx
@@ -4,29 +4,54 @@ import Button from '../../Misc/BasicButton/BasicButton';
 import './Form.scss'
 import BasicButton from '../../Misc/BasicButton/BasicButton';
 
-class ContactForm extends React.Component {
+interface State {
+    name: string
+    email: string
+    subject: string
+    message: string
+}
+
+type formName = 'name' | 'email' | 'subject' | 'message'
+
+class ContactForm extends React.Component<{}, State> {
+
+    state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    }
+
     render() {
         return (
             <Form className="contact-me-form">
                 <Row>
                     <Col sm={12}>
                         <Form.Group>
-                            <Form.Control placeholder="Name"/>
+                            <Form.Control name="name" value={this.state.name} placeholder="Name" onChange={(e) => {
+                                this.setState({name: e.target.value})
+                            }}/>
                         </Form.Group>
                     </Col>
                     <Col sm={6}>
                         <Form.Group>
-                            <Form.Control placeholder="Email"/>
+                            <Form.Control name="email" placeholder="Email" value={this.state.email} onChange={e => {
+                                this.setState({email: e.target.value})
+                            }}/>
                         </Form.Group>
                     </Col>
                     <Col sm={6}>
                         <Form.Group>
-                            <Form.Control placeholder="Subject"/>
+                            <Form.Control name="subject" placeholder="Subject" value={this.state.subject} onChange={e => {
+                                this.setState({subject: e.target.value})
+                            }}/>
                         </Form.Group>
                     </Col>
                     <Col sm={12}>
                         <Form.Group>
-                            <Form.Control as="textarea" rows="3" placeholder="Your message..."/>
+                            <Form.Control name="message" as="textarea" rows="3" placeholder="Your message..." value={this.state.message} onChange={e => {
+                                this.setState({message: e.target.value})
+                            }}/>
                         </Form.Group>
                     </Col>
                     <Col sm={12}>
